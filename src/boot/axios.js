4136e4e10e0af57c5215d87be54f29b975260bf7
Ryukikun <ridhodzaki03@gmail.com>
2020-07-16 09:54:24 +0700

    update

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.18.9:3000/'
+  baseURL: 'http://0dc1a04889bc.ngrok.io/'
 })
 export default async ({ Vue }) => {
   Vue.prototype.$axios = axiosInstance
